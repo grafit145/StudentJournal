29ab93941d1b87ca6a61253c9601b5e44ada3dcc
Anton Boriskin <antbor23@gmail.com>
2021-12-02 18:58:56 +0300

    add sql script

--- src/main/resources/database/InitClassesTable.sql
+++ src/main/resources/database/InitClassesTable.sql
@@ -3,7 +3,7 @@ DROP TABLE IF EXISTS classes;
 DROP SEQUENCE IF EXISTS classes_id_seq;
 CREATE TABLE IF NOT EXISTS classes
 (
-    class_id INTEGER PRIMARY KEY,
+    class_id   INTEGER PRIMARY KEY,
     class_name VARCHAR(10) UNIQUE NOT NULL
 );
 CREATE SEQUENCE classes_id_seq START WITH 1 INCREMENT BY 1;
@@ -13,11 +13,21 @@ DROP SEQUENCE IF EXISTS students_id_seq;
 CREATE TABLE IF NOT EXISTS students
 (
     student_id    INTEGER PRIMARY KEY,
-    firstname  VARCHAR(200) NOT NULL,
-    patronymic VARCHAR(200) NOT NULL,
-    surname VARCHAR(50)  NOT NULL,
-    date_of_birth date NOT NULL,
-    class_name VARCHAR(10) REFERENCES classes(class_name)NOT NULL
+    firstname     VARCHAR(200)                                NOT NULL,
+    patronymic    VARCHAR(200)                                NOT NULL,
+    surname       VARCHAR(50)                                 NOT NULL,
+    date_of_birth date                                        NOT NULL,
+    class_name    VARCHAR(10) REFERENCES classes (class_name) NOT NULL
 
 );
 CREATE SEQUENCE students_id_seq START WITH 1 INCREMENT BY 1;
+
+INSERT INTO classes (class_id, class_name)
+VALUES (1, '1a'),
+       (2, '1b');
+
+INSERT INTO students (student_id, firstname, patronymic, surname, date_of_birth, class_name)
+VALUES (1, 'Иванов', 'Иванович', 'Иванов', '2013-03-12', '1a'),
+ (2, 'Петр', 'Иванович', 'Иванов', '2013-03-12', '1a'),
+ (3, 'Виктория', 'Алексеевна', 'Петрова', '2013-09-29', '1b'),
+ (4, 'Олег', 'Алексеевич', 'Петров', '2014-02-03', '1b');
